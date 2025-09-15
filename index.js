652c894ec4a9cf51ecbc0036503da6cace698928
sanchitblyke <ssanchit960@gmail.com>
2025-09-15 13:43:17 +0530

    do it

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
